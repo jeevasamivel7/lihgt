79129bf680c63e073d5a299a39eb730c7d84111d
jeevasamivel7 <jeevasamivel7@gmail.com>
2021-10-11 15:59:18 +0530

    update

--- Model2/PRO/sam.js
+++
@@ -1,6 +0,0 @@
-var player = document.getElementById('player');    
-player.addEventListener("timeupdate", function() {
-    var currentTime = player.currentTime;
-    var duration = player.duration;
-    $('.hp_range').stop(true,true).animate({'width':(currentTime +.25)/duration*100+'%'},250,'linear');
-});
